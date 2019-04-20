267ea44cfbbb0101cc6acf49cc227a136c6f6a44
LiamLYJ <liuyongjie9313@yahoo.co.jp>
2019-04-20 21:09:34 +0900

    bug fixed for download

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://drive.google.com/open?id=1LdpB3nNl5oTxtmUW4IlNBGr8lSLD2JdA
+wget "https://drive.google.com/uc?export=download&id=1LdpB3nNl5oTxtmUW4IlNBGr8lSLD2JdA" -O model_800.ckpt 
