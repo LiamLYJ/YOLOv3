449e7336a8bb1bbd92613b00311c004d87977fcf
Wang Qianlong <wangqianlong120@gmail.com>
2019-05-01 00:18:54 +0900

    add float weights download

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 wget "https://drive.google.com/uc?export=download&id=1LdpB3nNl5oTxtmUW4IlNBGr8lSLD2JdA" -O model_800.ckpt 
+wget "https://drive.google.com/uc?export=download&id=1rMENsuXcZkKTZPBaiIGa66VLCsLJIKJt" -O model_300.ckpt
